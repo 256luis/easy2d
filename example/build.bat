2a2ce84c3980a70ba3e9489a5e04af20dfc8709d
luisbanez <luismbanez@gmail.com>
2024-04-27 01:24:29 +0800

    unimportant 2

---
+++ example/build.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set CC=cl
+set CFLAGS=/c /Fobuild\ /Fdbuild\ /O2 /Iinclude /std:c17 /D_CRT_SECURE_NO_WARNINGS /W4 /wd4255 /wd5105
+set SRC=*
+
+pushd ..
+
+call %CC% src/%SRC%.c %CFLAGS% /nologo
+
+if %errorlevel% == 0 (
+    if not exist lib mkdir lib
+    call lib build\*.obj /out:lib/easy2d.lib /nologo
+)
+
+popd
+
+set CFLAGS=/O2 /I..\include /std:c17 /D_CRT_SECURE_NO_WARNINGS /Wall /wd4255
+set LFLAGS=/link /libpath:..\lib easy2d.lib user32.lib gdi32.lib winmm.lib 
+set SRC=%1
+
+call %CC% %1.c %CFLAGS% %LFLAGS% /out:stars.exe /nologo
